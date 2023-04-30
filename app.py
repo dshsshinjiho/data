85c2d7162354e15988e7182b3f3662aebf032ee8
joysh <shinjiho2005@gmail.com>
2023-05-01 08:58:57 +0900

    123

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('streamlit 입문')
+st.header('This is a header')
+st.subheader('this is a subheader')
+st.text('hello world. streamlit text')
+name='shinjiho'
+st.text('hi,{}'.format(name))
+st.markdown("## This is markdown")
